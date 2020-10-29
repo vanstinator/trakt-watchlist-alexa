e3c7f1fcc8798e634f13e23accf589214e7f5f18
Justin Vanderhooft <justinvdhooft@gmail.com>
2020-10-29 08:33:34 -0400

    chore: update trakt env var

--- src/services/got.ts
+++ src/services/got.ts
@@ -4,7 +4,7 @@ const client = got.extend({
   headers: {
     'Content-Type': 'application/json',
     'trakt-api-version': '2',
-    'trakt-api-key': process.env.TRAKT_CLIENT,
+    'trakt-api-key': process.env.TRAKT_CLIENT_ID,
   }
 });
 
